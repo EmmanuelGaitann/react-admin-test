d5db181e0b83f6275f8267db4e77d65dcd91ac2a
EmmanuelGaitann <118638965+EmmanuelGaitann@users.noreply.github.com>
2025-02-01 03:42:33 +0100

    ;

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { ReferenceInput } from 'react-admin';
-import { Admin, Resource, List, Datagrid, TextField, DateField, ReferenceField, EditButton, SelectInput, Filter } from 'react-admin';
+import { Admin, Resource, List, Datagrid, TextField, DateField, ReferenceField, EditButton, SelectInput, Filter, TextInput, ReferenceInput, Edit, SimpleForm, TextInput as RAInput, DateInput } from 'react-admin'; 
 import jsonServerProvider from 'ra-data-json-server';
 
 // Définir le filtre pour les posts
 const PostFilter = (props) => (
     <Filter {...props}>
+        <TextInput label="Search by title" source="q" alwaysOn />
         <ReferenceInput label="Author" source="userId" reference="users">
             <SelectInput optionText="name" />
         </ReferenceInput>
@@ -22,8 +22,8 @@ const PostFilter = (props) => (
 
 // Définir la vue List pour les posts
 const PostList = (props) => (
-    <List {...props} filters={<PostFilter />} perPage={25}>
-        <Datagrid>
+    <List {...props} filters={<PostFilter />} perPage={25} pagination={true} sort={{ field: 'publishedAt', order: 'DESC' }}>
+        <Datagrid rowClick="edit">
             <TextField source="title" />
             <ReferenceField label="Author" source="userId" reference="users">
                 <TextField source="name" />
@@ -35,13 +35,32 @@ const PostList = (props) => (
     </List>
 );
 
+// Définir la vue Edit pour les posts
+const PostEdit = (props) => (
+    <Edit {...props}>
+        <SimpleForm>
+            <RAInput label="Title" source="title" />
+            <SelectInput
+                label="Status"
+                source="status"
+                choices={[
+                    { id: 'published', name: 'Published' },
+                    { id: 'draft', name: 'Draft' },
+                ]}
+            />
+            <DateInput label="Published At" source="publishedAt" />
+        </SimpleForm>
+    </Edit>
+);
+
 const dataProvider = jsonServerProvider('http://localhost:5000');
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
     <Resource name="users" />
-    <Resource name="posts" list={PostList} />
+    <Resource name="posts" list={PostList} edit={PostEdit} />
   </Admin>
 );
 
 export default App;
+
